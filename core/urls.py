ca1af9402b734eef49bc5002f10c516140cd2027
Gary Desktop <frublob46@gmail.com>
2022-10-15 15:46:12 +0100

    Set AullAuth configuration options and lightly customised templates.

--- core/urls.py
+++ core/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('allauth.urls')), # AllAuth Required NB. Not default of 'accounts/'
     path('', include('your_app.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')), # AllAuth Required
 ]
--- your_app/urls.py
+++ your_app/urls.py
@@ -8,5 +8,6 @@ app_name = 'your_app'
 urlpatterns = [
     # Home Page
     path('', views.index, name='index'),
+    # Home Page again with optional parm, I don't believe in esoteric code (regex)
     path('<slug:option>', views.index, name='index'),
 ]

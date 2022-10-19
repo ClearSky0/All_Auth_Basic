4ed331160960575f2da9a03492710449690037f1
Gary Desktop <frublob46@gmail.com>
2022-10-19 18:01:56 +0100

    Moved 'account' into core with base. Removed 'Members Interest' from a comment.

--- your_app/views.py
+++ your_app/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request, option='Home'):
-    """The home page for Members Interests"""
+    """The home page for Your Site"""
     page_title = option
     return render(request, "index.html", {'page_title': page_title} )

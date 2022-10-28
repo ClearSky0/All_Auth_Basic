45ba21769e50b9acabdc2b7f56b448016fcd9e3f
ClearSky0 <58333103+ClearSky0@users.noreply.github.com>
2022-10-28 11:37:46 +0100

    Added dropdown on profile and fixed-to-top on navbar, & code formatting changes.

--- your_app/views.py
+++ your_app/views.py
@@ -5,4 +5,14 @@ from django.shortcuts import render
 def index(request, option='Home'):
     """The home page for Your Site"""
     page_title = option
-    return render(request, "index.html", {'page_title': page_title} )
+
+    if option == 'Option3':
+        long_content = "Page filler for testing fixed navbar." * 80
+    else:
+        long_content = ""
+
+    context = {
+        'page_title': page_title,
+        'long_content': long_content,
+    }
+    return render(request, "index.html", context )
